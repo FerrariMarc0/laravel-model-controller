764f3ff0848b8da5a50a65887e85fa45156acdce
FerrariMarc0 <marco1010@live.it>
2023-05-04 17:23:58 +0200

    db connected

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,5 +8,15 @@
 </head>
 <body>
     <h1>Hello</h1>
+
+    @foreach ($movies as $movie)
+        <div class="card">
+            <ul>
+                <li>{{$movie->title}}</li>
+            </ul>
+        </div>
+    @endforeach
+
+
 </body>
 </html>
