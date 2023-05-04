fba621d2f5cf5898c7b6df5c1d549b99d43dafb7
FerrariMarc0 <marco1010@live.it>
2023-05-04 18:25:15 +0200

    cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    @vite('resources/scss/style.scss')
+    <title>laravel-model-controller</title>
 </head>
 <body>
-    <h1>Hello</h1>
-
+    <div class="container">
     @foreach ($movies as $movie)
-        <div class="card">
-            <ul>
-                <li>{{$movie->title}}</li>
-            </ul>
-        </div>
-    @endforeach
 
+            <div class="card">
+                <h2>Title: {{$movie->title}}</h2>
+                <h3>Original title: {{$movie->original_title}}</h3>
+                <span>Nationality: {{$movie->nationality}}</span>
+                <span>Release date: {{$movie->date}}</span>
+                <span>Vote: {{$movie->vote}}</span>
+            </div>
+
+    @endforeach
+    </div>
 
 </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel([
-            'resources/css/app.css',
+            'resources/scss/style.scss',
             'resources/js/app.js',
         ]),
     ],
